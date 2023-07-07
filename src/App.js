55be68b2a3eccd350075c565643e7a8259c702a4
Ashok Kandpal <ashokkandpal845@gmail.com>
2023-07-07 20:47:09 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import React from 'react';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import { BrowserRouter , Route , Routes } from 'react-router-dom';
+import Contact from './Components/Contact';
+import About from './Components/About';
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+  );
+};
+
+export default App;
